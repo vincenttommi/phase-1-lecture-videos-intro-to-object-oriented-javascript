04a907de16380b237e96a3234b3215be4dc8697e
vincenttommi <vincenttommi@gmail.com>
2023-03-29 21:27:31 +0300

    more classes in js

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
-class Cat {
+// class Cat {
 
-//blue print fo an object
+// //blue print fo an object
 
-//to initialise a  an object we use a constructor
+// //to initialise a  an object we use a constructor
 
-constructor(name, breed,type,age){
+// constructor(name, breed,type,age){
 
-    //assiging values to objects
-    this.name = name
-    this.breed = breed
-    this.type = type
-    this.age = age
+//     //assiging values to objects
+//     this.name = name
+//     this.breed = breed
+//     this.type = type
+//     this.age = age
 
-    console.log(this)
+//     console.log(this)
     
 
+// }
+// }
+
+// new Cat("rose","domestic","longair",9)
+// new Cat("puppy","local","shorthair",20)
+
+
+
+
+class Cars{
+
+constructor(type,model,price, country){
+
+
+    this.type = type
+    this.model = model
+    this.price  = price
+    this.country = country
+
+    console.log(this)
+
+
 }
 }
 
-new Cat("rose","domestic","longair",9)
-new Cat("puppy","local","shorthair",20)
+
+new Cars("lexus","2020",20000000, "germany")
+new Cars("Ecalade",2021, 30000000,"spain")
